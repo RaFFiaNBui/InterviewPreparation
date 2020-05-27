75023ecd07ff94daff594706df485e93aaea2a6b
RaFFiaNBui <Raffian_bui@mail.ru>
2020-05-27 15:53:04 +0300

    ДЗ урок 3

---
+++ Lesson3/MyList.java
@@ -0,0 +1,67 @@
+package thrid;
+
+import java.util.Iterator;
+
+public class MyList<E> implements Iterable<E> {
+    private Node<E> node = null;
+    private int counter = 0;
+
+    public MyList() {
+        node = new Node<>();
+    }
+
+    public int size() {
+        return counter;
+    }
+
+    public void add(E element) {
+        if (counter == 0) {
+            node.set(element);
+        } else {
+            node.add(element);
+        }
+        counter++;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new MyIterator(node);
+    }
+
+    private class Node<E> {
+        private E element;
+        private Node<E> next = null;
+        private Node<E> prev = null;
+
+        Node() {}
+        Node(E element) {
+            set(element);
+        }
+
+        void set(E element) {
+            this.element = element;
+        }
+
+        void add(E element) {
+            if (isNext()) {
+                next.add(element);
+            } else {
+                E temp = this.element;
+                next = new Node<>(element);
+                next.setPrev(temp);
+            }
+        }
+
+        E get() {
+            return element;
+        }
+
+        boolean isNext() {
+            return next != null;
+        }
+
+        void setPrev (E element) {
+            prev = this;
+        }
+    }
+}
