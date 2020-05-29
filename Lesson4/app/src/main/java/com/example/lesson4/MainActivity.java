c69cab8aa672145547b392d6ba7921591da40398
RaFFiaNBui <Raffian_bui@mail.ru>
2020-05-29 22:48:24 +0300

    Completed paragraph 1 of homework for lesson 4

--- Lesson4/app/src/main/java/com/example/lesson4/MainActivity.java
+++ Lesson4/app/src/main/java/com/example/lesson4/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.lesson4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LifeCycleActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
+        if (savedInstanceState == null) {
+            MyFragment myFragment = new MyFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.fragment, myFragment);
+            ft.commit();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 }
